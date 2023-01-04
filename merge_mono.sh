5ed8fbd5124fc69915ad977f2a48eddd21d0de93
HalimZaki <a.halimzaki@gmail.com>
2023-01-04 19:04:20 +0200

    Adding mono script

---
+++ merge_mono.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+
+# Echo commands
+
+set -o xtrace
+
+
+
+GIT_HOME="git@github.com"
+BRANCH="main"
+
+
+
+for repo in "${@:1}"
+
+do 
+
+	IFS="/" read owner repo_name <<<"$repo"
+
+	git remote add -f $repo_name $GIT_HOME:${owner}/${repo_name}.git
+
+	git merge --no-commit --no-ff $repo_name/$BRANCH --allow-unrelated-histories
+
+	git add .
+
+	git commit -m "<Merge $repo_name to main files>"
+
+done
